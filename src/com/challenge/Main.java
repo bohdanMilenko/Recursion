c5002b9dcf697b9d133eaad3b603531b97d9faf0
Bohdan Milenko <bogdan57milenko@gmail.com>
2019-12-22 23:40:00 -0500

    Changed logic of the recursion

--- src/com/challenge/Main.java
+++ src/com/challenge/Main.java
@@ -4,7 +4,8 @@ import UserInputUtil.UserInput;
 
 public class Main {
 
-    private static int n = 0;
+    private static int n = 1;
+    private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
 
     String stringFromUser = UserInput.getStringFromUser();
@@ -14,14 +15,15 @@ public class Main {
 
 
     public static String shortenTheWord(String inputString){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < inputString.length();i++){
+        for(int i = 0; i < inputString.length()-1;i++){
             if(inputString.charAt(i) == inputString.charAt(i+1)){
                 n++;
-                System.out.println(inputString.substring(i+n-1));
-                shortenTheWord(inputString.substring(i+n-1));
+                System.out.println("i = " + i);
+                System.out.println("n = " + n);
+                System.out.println(inputString.substring(i+1));
+                shortenTheWord(inputString.substring(i+1));
             }else {
-                if(n>0) {
+                if(n>1) {
                     sb.append(inputString.charAt(i)).append(n);
                     System.out.println(sb.toString());
                 }else {
@@ -31,6 +33,17 @@ public class Main {
                 n=0;
             }
         }
+        System.out.println(sb.toString());
         return sb.toString();
+    }
+
+    public static String shortedTheWord2(String inputString,int occurance){
+        if(inputString.charAt(1) == inputString.charAt(2)) {
+            n++;
+            System.out.println("String: " + inputString.substring(1));
+            if (inputString.substring(1).length() == 1 ) return "";
+            return shortedTheWord2(inputString.substring(1),n);
+        }
+        return sb.append();
     }
 }
